4a92274fba29501066b6641c40ee89c7f8958747
MM <mmendrygal@queris.pl>
2017-08-14 09:17:26 +0200

    Paths textboxes and functionality, implemented flag to stop the code after path not found break, code cleanup

--- Config_Util/Config_Util/MainWindow.xaml.cs
+++ Config_Util/Config_Util/MainWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.IO;
 using System.Xml;
 using System.Text.RegularExpressions;
+using Microsoft.Web.Administration;
 using System.Collections.Specialized;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -32,20 +33,27 @@ namespace Config_Util
 
         public void LoadFilesButton_Click(object sender, RoutedEventArgs e)
         {
+            LoadFilesButton.Content = "Loading...";
             string CMMSPathVar = CMMSPath.Text;
             if (string.IsNullOrEmpty(CMMSPathVar))
             {
                 MessageBox.Show("Enter CMMS Path first!", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                LoadFilesButton.Content = "Load CMMS Catalogue";
             }
             else
             {
                 if (Directory.Exists(CMMSPathVar))
                 {
+                    bool GetServiceSuccess = false;
                     string ServiceConfigPath = string.Empty;
                     string RestConfigPath = string.Empty;
                     string WebConfigPath = string.Empty;
                     string ClientConfigPath = string.Empty;
                     string ExePath = string.Empty;
+                    string ServicePath = string.Empty;
+                    string RestPath = string.Empty;
+                    string WebPath = string.Empty;
+                    string ClientPath = string.Empty;
                     string NewServicePath = CMMSPathVar + "\\Service\\";
                     string OldServicePath = CMMSPathVar + "\\RRM3Services\\";
                     string NewRestPath = CMMSPathVar + "\\RestService\\";
@@ -56,88 +64,133 @@ namespace Config_Util
                     string OldClientPath = CMMSPathVar + "\\RRM3Client\\";
                     string NewExePath = CMMSPathVar + "\\Client\\RRM3.exe";
                     string OldExePath = CMMSPathVar + "\\RRM3Client\\RRM3.exe";
+                    string PanelPath = CMMSPathVar + "\\Panel\\";
 
                     if (Directory.Exists(NewServicePath))
                     {
+                        ServiceBox.Text = NewServicePath;
                         ServiceConfigPath = NewServicePath + "Web.config";
+                        GetServiceSuccess = true;
                     }
                     else if (Directory.Exists(OldServicePath))
                     {
+                        ServiceBox.Text = OldServicePath;
                         ServiceConfigPath = OldServicePath + "Web.config";
+                        GetServiceSuccess = true;
                     }
                     else
                     {
                         MessageBox.Show("Service directory not found! Check if CMMS path is correct.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        LoadFilesButton.Content = "Load CMMS Catalogue";
                     }
                     if (Directory.Exists(NewRestPath))
                     {
+                        RestBox.Text = NewRestPath;
                         RestConfigPath = NewRestPath + "Web.config";
                     }
                     else if (Directory.Exists(OldRestPath))
                     {
+                        RestBox.Text = NewRestPath;
                         RestConfigPath = OldRestPath + "Web.config";
                     }
                     else
                     {
-                        MessageBox.Show("Rest Service directory not found! Check if CMMS path is correct.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        RestBox.Text = "Rest not found";
                     }
                     if (Directory.Exists(NewWebPath))
                     {
+                        WebBox.Text = NewWebPath;
                         WebConfigPath = NewWebPath + "Web.config";
                     }
                     else if (Directory.Exists(OldWebPath))
                     {
+                        WebBox.Text = OldWebPath;
                         WebConfigPath = OldWebPath + "Web.config";
                     }
                     else
                     {
-                        MessageBox.Show("Web Service directory not found! Check if CMMS path is correct.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        WebBox.Text = "Web not found";
                     }
                     if (Directory.Exists(NewClientPath))
                     {
-                        ClientConfigPath = NewClientPath + "Web.config";
+                        ClientBox.Text = NewClientPath;
+                        ClientConfigPath = NewClientPath + "RRM3.exe.config";
                         ExePath = NewClientPath + "RRM3.exe";
                     }
                     else if (Directory.Exists(OldClientPath))
                     {
-                        ClientConfigPath = OldClientPath + "Web.config";
+                        ClientBox.Text = OldClientPath;
+                        ClientConfigPath = OldClientPath + "RRM3.exe.config";
                         ExePath = OldClientPath + "RRM3.exe";
                     }
                     else
                     {
-                        MessageBox.Show("Client directory not found! Check if CMMS path is correct.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        ClientBox.Text = "Client not found";
                     }
-                    XmlDocument config = new XmlDocument();
-                    config.Load(ServiceConfigPath);
-                    XmlNode node = config.DocumentElement.SelectSingleNode("/configuration/connectionStrings/add");
-                    string ConnectionString = node.Attributes["connectionString"].Value;
-                    Regex regex = new Regex("=(\\w.*?);");
-                    MatchCollection result = regex.Matches(ConnectionString);
-                    AddressBox.Text = result[2].ToString();
-                    AddressBox.Text = AddressBox.Text.Substring(1, AddressBox.Text.Length - 2);
-                    NameBox.Text = result[3].ToString();
-                    NameBox.Text = NameBox.Text.Substring(1, NameBox.Text.Length - 2);
-                    LoginBox.Text = result[5].ToString();
-                    LoginBox.Text = LoginBox.Text.Substring(1, LoginBox.Text.Length - 2);
-                    PasswordBox.Text = result[6].ToString();
-                    PasswordBox.Text = PasswordBox.Text.Substring(1, PasswordBox.Text.Length - 2);
-                    AddressBox.Visibility = System.Windows.Visibility.Visible;
-                    NameBox.Visibility = System.Windows.Visibility.Visible;
-                    LoginBox.Visibility = System.Windows.Visibility.Visible;
-                    PasswordBox.Visibility = System.Windows.Visibility.Visible;
-                    VersionBox.Visibility = System.Windows.Visibility.Visible;
-                    AddressLabel.Visibility = System.Windows.Visibility.Visible;
-                    NameLabel.Visibility = System.Windows.Visibility.Visible;
-                    LoginLabel.Visibility = System.Windows.Visibility.Visible;
-                    PasswordLabel.Visibility = System.Windows.Visibility.Visible;
-                    VersionLabel.Visibility = System.Windows.Visibility.Visible;
-                    ListFilesButton.IsEnabled = true;
-                    EditButton.IsEnabled = true;
-                    LoadDBButton.IsEnabled = true;
+                    if (Directory.Exists(PanelPath))
+                    {
+                        PanelBox.Text = PanelPath;
+                    }
+                    else
+                    {
+                        PanelBox.Text = "Panel not found.";
+                    }
+                    if (GetServiceSuccess == true)
+                    {
+                        XmlDocument config = new XmlDocument();
+                        XmlDocument clientconfig = new XmlDocument();
+                        clientconfig.Load(ClientConfigPath);
+                        config.Load(ServiceConfigPath);
+                        XmlNode client_node = clientconfig.DocumentElement.SelectSingleNode("/configuration/appSettings/add");
+                        XmlNode node = config.DocumentElement.SelectSingleNode("/configuration/connectionStrings/add");
+                        string ClientVersion = client_node.Attributes["value"].Value;
+                        string ConnectionString = node.Attributes["connectionString"].Value;
+                        Regex regex = new Regex("=(\\w.*?);");
+                        MatchCollection result = regex.Matches(ConnectionString);
+                        AddressBox.Text = result[2].ToString();
+                        AddressBox.Text = AddressBox.Text.Substring(1, AddressBox.Text.Length - 2);
+                        NameBox.Text = result[3].ToString();
+                        NameBox.Text = NameBox.Text.Substring(1, NameBox.Text.Length - 2);
+                        LoginBox.Text = result[5].ToString();
+                        LoginBox.Text = LoginBox.Text.Substring(1, LoginBox.Text.Length - 2);
+                        PasswordBox.Text = result[6].ToString();
+                        PasswordBox.Text = PasswordBox.Text.Substring(1, PasswordBox.Text.Length - 2);
+                        VersionBox.Text = ClientVersion;
+                        AddressBox.Visibility = System.Windows.Visibility.Visible;
+                        NameBox.Visibility = System.Windows.Visibility.Visible;
+                        LoginBox.Visibility = System.Windows.Visibility.Visible;
+                        PasswordBox.Visibility = System.Windows.Visibility.Visible;
+                        VersionBox.Visibility = System.Windows.Visibility.Visible;
+                        ServiceBox.Visibility = System.Windows.Visibility.Visible;
+                        ClientBox.Visibility = System.Windows.Visibility.Visible;
+                        RestBox.Visibility = System.Windows.Visibility.Visible;
+                        WebBox.Visibility = System.Windows.Visibility.Visible;
+                        PanelBox.Visibility = System.Windows.Visibility.Visible;
+                        AddressLabel.Visibility = System.Windows.Visibility.Visible;
+                        NameLabel.Visibility = System.Windows.Visibility.Visible;
+                        LoginLabel.Visibility = System.Windows.Visibility.Visible;
+                        PasswordLabel.Visibility = System.Windows.Visibility.Visible;
+                        VersionLabel.Visibility = System.Windows.Visibility.Visible;
+                        ServiceLabel.Visibility = System.Windows.Visibility.Visible;
+                        ClientLabel.Visibility = System.Windows.Visibility.Visible;
+                        RestLabel.Visibility = System.Windows.Visibility.Visible;
+                        WebLabel.Visibility = System.Windows.Visibility.Visible;
+                        PanelLabel.Visibility = System.Windows.Visibility.Visible;
+                        ListFilesButton.IsEnabled = true;
+                        EditButton.IsEnabled = true;
+                        LoadDBButton.IsEnabled = true;
+                        LoadFilesButton.Content = "Reload CMMS Catalogue";
+                    }
+                    else
+                    {
+                        MessageBox.Show("Directory not found. Make sure path is correct.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                        LoadFilesButton.Content = "Load CMMS Catalogue";
+                    }
                 }
                 else
                 {
                     MessageBox.Show("Directory not found. Make sure path is correct.", "Error!", MessageBoxButton.OK, MessageBoxImage.Error);
+                    LoadFilesButton.Content = "Load CMMS Catalogue";
                 }
                 
             }
